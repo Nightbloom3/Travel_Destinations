2c19caeaf328e79d23c0989e7973ca9d6416beaa
Nikolaj Larsen <56347460+Nearial@users.noreply.github.com>
2022-10-13 09:49:15 +0200

    Initial Edit Fix

--- update_script_mongoose.js
+++ update_script_mongoose.js
@@ -1,35 +1,27 @@
 const template = document.querySelector("#update-travel-form");
 
-function update(travel) {
-    const id = travel._id;
-    const location = travel.location;
-    console.log(id);
-    console.log(location);
-}
-
 template.addEventListener("submit", async (event) => {
-    event.preventDefault();
+  event.preventDefault();
 
-    const travel = {
-        country: document.querySelector("#country").value,
-        location: document.querySelector("#location").value,
-        period: document.querySelector("#period").value,
-        description: document.querySelector("#description").value,
-    };
+  const travel = {
+    country: document.querySelector("#country").value,
+    location: document.querySelector("#location").value,
+    period: document.querySelector("#period").value,
+    description: document.querySelector("#description").value,
+  };
 
-    const response = await putData(travel);
-    console.log(response);
-    console.log(travel);
+  const response = await putData(travel);
+  console.log(response);
+  console.log(travel);
 });
 
 async function putData(id) {
-    const response = await fetch(url, {
-        method: "PUT",
-        headers: {"Content-Type": "application/json",
-    },
+  const response = await fetch(url, {
+    method: "PUT",
+    headers: { "Content-Type": "application/json" },
     // change back to --         body: JSON.stringify({travel}),
-        body: JSON.stringify(travel),
-    })
-        console.log(travel);
-    return response;
-};
+    body: JSON.stringify(travel),
+  });
+  console.log(travel);
+  return response;
+}
