7d22cff32cc928e0c261a9ec9711344c8c5f2091
ChristianLind <clind_14@hotmail.com>
2022-10-13 09:45:14 +0200

    folder

--- update_script_mongoose.js
+++ update_script_mongoose.js
@@ -2,25 +2,23 @@ const template = document.querySelector("#update-travel-form");
 
 function update(travel) {
     const id = travel._id;
-    const location = travel.location;
     console.log(id);
-    console.log(location);
 }
 
-template.addEventListener("submit", async (event) => {
-    event.preventDefault();
+// template.addEventListener("submit", async (event) => {
+//     event.preventDefault();
 
-    const travel = {
-        country: document.querySelector("#country").value,
-        location: document.querySelector("#location").value,
-        period: document.querySelector("#period").value,
-        description: document.querySelector("#description").value,
-    };
+//     const travel = {
+//         country: document.querySelector("#country").value,
+//         location: document.querySelector("#location").value,
+//         period: document.querySelector("#period").value,
+//         description: document.querySelector("#description").value,
+//     };
 
-    const response = await putData(travel);
-    console.log(response);
-    console.log(travel);
-});
+//     const response = await putData(travel);
+//     console.log(response);
+//     console.log(travel);
+// });
 
 async function putData(id) {
     const response = await fetch(url, {
